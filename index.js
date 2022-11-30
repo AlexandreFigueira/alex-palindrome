a8628b80c237d1f60424157e8ac8237f5367f4f0
AlexandreFigueira <figueira.alexandre@gmail.com>
2022-11-30 07:54:52 -0300

    Adicionando index.js.

---
+++ index.js
@@ -0,0 +1,8 @@
+function reverse(string) {
+    return string.split('').reverse().join('');
+}
+
+function palindrome(string) {
+    let processedContent = string.toLowerCase();
+    return processedContent === reverse(processedContent);
+}
