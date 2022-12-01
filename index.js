57a2db4e0ca37db3abe3b2abbcd982ca4f0ea4eb
AlexandreFigueira <figueira.alexandre@gmail.com>
2022-12-01 15:24:11 -0300

    Finish working and refactored palindrome method

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
-function reverse(string) {
-    return string.split('').reverse().join('');
+module.exports = Phrase;
+
+// Adds `reverse` to all strings.
+String.prototype.reverse = function() {
+  return Array.from(this).reverse().join("");
 }
 
-function palindrome(string) {
-    let processedContent = string.toLowerCase();
-    return processedContent === reverse(processedContent);
+// Defines a Phrase object.
+function Phrase(content) {
+  this.content = content;
+
+  // Returns content processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.letters().toLowerCase();
+  }
+
+  // Returns the letters in the content.
+  this.letters = function letters() {
+    return (this.content.match(/[a-zA-Z]/g) || []).join('');
+  }
+
+  // Returns true if the phrase is a palindrome, false otherwise.
+  this.palindrome = function palindrome() {
+    return this.processedContent() === this.processedContent().reverse();
+  }
+
 }
